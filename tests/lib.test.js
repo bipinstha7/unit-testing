7ea79e58371cb90d6a9a9016bcec54d73899ba6a
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-09 09:22:09 +0545

    arrays test

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -25,3 +25,29 @@ describe("greet", () => {
     expect(result).toContain("Bipin");
   });
 });
+
+
+describe("getCurrencies", () => {
+  it("should return supported currencies", () => {
+    const result = lib.getCurrencies();
+
+    // too genera
+    expect(result).toBeDefined();
+    expect(result).not.toBeNull();
+
+    // too specific
+    expect(result[0]).toBe("USD");
+    expect(result[1]).toBe("AUD");
+    expect(result[2]).toBe("EUR");
+    expect(result.length).toBe(3);
+
+    // Proper way
+    expect(result).toContain("USD");
+    expect(result).toContain("AUD");
+    expect(result).toContain("EUR");
+
+     // Ideal way
+     expect(result).toEqual(expect.arrayContaining(["EUR", "USD", "AUD"]));
+
+  }); 
+});
