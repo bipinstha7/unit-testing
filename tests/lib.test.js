f9d3410fba00f43809d755807065e089d58cc800
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-06 21:48:32 +0545

    Test absoulte function

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,16 @@
-test('Check number', () => {
-  
+const lib = require('../lib')
+
+test('absolute - should return a positive number if input is positive', () => {
+	const result = lib.absolute(2)
+	expect(result).toBe(2)
+})
+
+test('absolute - should return a positive number if input is negative', () => {
+	const result = lib.absolute(-2)
+	expect(result).toBe(2)
+})
+
+test('absolute - should return 0 number if input is 0', () => {
+	const result = lib.absolute(0)
+	expect(result).toBe(0)
 })
