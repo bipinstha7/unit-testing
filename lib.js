a40290335c5ecee0b368c69c9c7bc85d54cd048b
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-06 21:41:16 +0545

    Testing Files

--- lib.js
+++ lib.js
@@ -3,7 +3,9 @@ const mail = require('./mail');
 
 // Testing numbers 
 module.exports.absolute = function(number) {
-  return (number >= 0) ? number : -number;
+  if (number > 0) return number; 
+  if (number < 0) return -number; 
+  return 0; 
 }
 
 // Testing strings 
