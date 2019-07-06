a0e59a02aca75594fc6f431f405f3aa31993c083
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-06 22:50:22 +0545

    Test error and object

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -58,3 +58,20 @@ describe('getProduct', () => {
 		expect(result).toHaveProperty('id', 1)
 	})
 })
+
+describe('registerUser', () => {
+	it('should throw if username is falsy', () => {
+		const args = [null, undefined, NaN, '', 0, false]
+		args.forEach(a => {
+			expect(() => {
+				lib.registerUser(a)
+			}).toThrow()
+		})
+	})
+
+	it('should return if valid username is passed', () => {
+		const resutl = lib.registerUser('Node')
+		expect(result).toMatchObject({ username: 'Node' })
+		expect(result.id).toBeGreaterThan(0)
+	})
+})
